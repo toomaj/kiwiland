ae245f4538886a7e21e14d268d410a6ed995edcf
toomaj <t.boloorian@gmail.com>
2016-11-14 17:40:04 +1000

    Implement very basic RailInfoComponent and template
    ModuleID is set to module.id to facilitate relative assets for RailInfoComponent

---
+++ app/rail-info/rail-info.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rail-info',
+  templateUrl: 'rail-info.component.html'
+})
+export class RailInfoComponent { }
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { browser, element, by } from 'protractor';
 
 describe('Kiwiland E2E Tests', function () {
   let pageHeading = "This is Kiwiland's Home Page";
+  let railInfoHeading = "Rail Info";
 
   beforeEach(function () {
     browser.get('');
@@ -9,5 +10,9 @@ describe('Kiwiland E2E Tests', function () {
 
   it('should display: ' + pageHeading, function () {
     expect(element(by.css('h3')).getText()).toEqual(pageHeading);
+  });
+
+  it('should display: ' + railInfoHeading, function () {
+    expect(element(by.css('div')).getText()).toEqual(railInfoHeading);
   });
 });
